16b4a32ae7b7598fede1e24bcd9796f6e2e73483
Gaurav kumar <38521462+kumargaurav05@users.noreply.github.com>
2021-09-03 14:26:18 +0530

    deleted useless commets

--- mars.js
+++ mars.js
@@ -816,8 +816,7 @@ function getViewportDimensions() {
     // We execute the same script as before
     let vh = window.innerHeight * 0.01;
     let vw = window.innerWidth * 0.01;
-    // console.log(vh);
-    // console.log(vw);
+    
     document.documentElement.style.setProperty("--vh", `${vh}px`);
     document.documentElement.style.setProperty("--vw", `${vw}px`);
   });
